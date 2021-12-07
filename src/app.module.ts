827e5803c47ac8dc455342a2724c99647531cc0b
lordvidex <lordvidex.prime@gmail.com>
2021-12-07 21:13:41 +0300

    fix 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,21 @@ import { OfficesModule } from './offices/offices.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { VoteModule } from './vote/vote.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
-    MongooseModule.forRoot('mongodb://localhost:27017/anssir'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('DATABASE_URL') || 'mongodb://localhost:27017/anssir',
+      }),
+    }),
     OfficesModule,
     VoteModule,
-  ]
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors()
 
   // swagger docs configs
   const PORT = process.env.PORT || 3000;
   const swaggerConfig = new DocumentBuilder()
     .setTitle('ANSSIR voting API')
     .setDescription('Secure API for ANSSIR 2022 voting')
-    .addServer(`http://localhost:${PORT}`)
-    .addServer(process.env.HOST)
+    .addServer(`http://localhost:${PORT}`, 'Local Server')
+    .addServer(process.env.HOST, 'Production Server')
     // .addBearerAuth()
     .build();
 
   const doc = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, doc);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
